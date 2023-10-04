9453274bf8f58e46c087dbf28dacb70963d410a5
Surya <49099412+suryapandiaraj@users.noreply.github.com>
2023-10-04 22:35:31 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -18,7 +18,7 @@ public class Main
     }
     public static class B extends A {
         public String meth(){
-            System.out.println(super.meth());
+            //System.out.println(super.meth());
             return "Method is overridden in Extendend class B";
         }
     }
